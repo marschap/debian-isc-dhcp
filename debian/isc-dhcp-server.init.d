ee3dffdda38a8cfc6ad2005d8d64a165d2a709ba
Michael Gilbert <mgilbert@debian.org>
2016-01-09 20:44:28 +0000

    release 4.3.3-6

--- debian/isc-dhcp-server.init.d
+++ debian/isc-dhcp-server.init.d
@@ -1,6 +1,4 @@
 #!/bin/sh
-#
-#
 
 ### BEGIN INIT INFO
 # Provides:          isc-dhcp-server
@@ -34,72 +32,123 @@ fi
 # Read init script configuration
 [ -f "$DHCPD_DEFAULT" ] && . "$DHCPD_DEFAULT"
 
-NAME=dhcpd
-DESC="ISC DHCP server"
-# fallback to default config file
-DHCPD_CONF=${DHCPD_CONF:-/etc/dhcp/dhcpd.conf}
-# try to read pid file name from config file, with fallback to /var/run/dhcpd.pid
-if [ -z "$DHCPD_PID" ]; then
-	DHCPD_PID=$(sed -n -e 's/^[ \t]*pid-file-name[ \t]*"\(.*\)"[ \t]*;.*$/\1/p' < "$DHCPD_CONF" 2>/dev/null | head -n 1)
+NAME4=dhcpd
+NAME6=dhcpd6
+
+DESC4="ISC DHCPv4 server"
+DESC6="ISC DHCPv6 server"
+
+# use already specified config file or fallback to defaults
+DHCPDv4_CONF=${DHCPDv4_CONF:-/etc/dhcp/dhcpd.conf}
+DHCPDv6_CONF=${DHCPDv6_CONF:-/etc/dhcp/dhcpd.conf}
+
+# try to read pid file name from config file or fallback to defaults
+if [ -z "$DHCPDv4_PID" ]; then
+	DHCPDv4_PID=$(sed -n -e 's/^[ \t]*pid-file-name[ \t]*"\(.*\)"[ \t]*;.*$/\1/p' < "$DHCPDv4_CONF" 2>/dev/null | head -n 1)
 fi
-DHCPD_PID="${DHCPD_PID:-/var/run/dhcpd.pid}"
+if [ -z "$DHCPDv6_PID" ]; then
+	DHCPDv6_PID=$(sed -n -e 's/^[ \t]*pid-file-name[ \t]*"\(.*\)"[ \t]*;.*$/\1/p' < "$DHCPDv6_CONF" 2>/dev/null | head -n 1)
+fi
+DHCPDv4_PID="${DHCPDv4_PID:-/var/run/dhcpd.pid}"
+DHCPDv6_PID="${DHCPDv4_PID:-/var/run/dhcpd6.pid}"
 
 test_config()
 {
-	if ! /usr/sbin/dhcpd -t $OPTIONS -q -cf "$DHCPD_CONF" > /dev/null 2>&1; then
-		echo "dhcpd self-test failed. Please fix $DHCPD_CONF."
+        VERSION="$1"
+	CONF="$2"
+
+	if ! /usr/sbin/dhcpd -t $VERSION -q -cf "$CONF" > /dev/null 2>&1; then
+		echo "dhcpd self-test failed. Please fix $CONF."
 		echo "The error was: "
-		/usr/sbin/dhcpd -t $OPTIONS -cf "$DHCPD_CONF"
+		/usr/sbin/dhcpd -t $VERSION -cf "$CONF"
 		exit 1
 	fi
-	touch /var/lib/dhcp/dhcpd.leases
 }
 
-# single arg is -v for messages, -q for none
 check_status()
 {
-    if [ ! -r "$DHCPD_PID" ]; then
-	test "$1" != -v || echo "$NAME is not running."
-	return 3
-    fi
-    if read pid < "$DHCPD_PID" && ps -p "$pid" > /dev/null 2>&1; then
-	test "$1" != -v || echo "$NAME is running."
-	return 0
-    else
-	test "$1" != -v || echo "$NAME is not running but $DHCPD_PID exists."
-	return 1
-    fi
+        OPTION="$1"
+        PIDFILE="$2"
+        NAME="$3"
+
+        if [ ! -r "$PIDFILE" ]; then
+                test "$OPTION" != -v || echo "$NAME is not running."
+		return 3
+        fi
+
+        if read pid < "$PIDFILE" && ps -p "$pid" > /dev/null 2>&1; then
+		test "$OPTION" != -v || echo "$NAME is running."
+		return 0
+        else
+		test "$OPTION" != -v || echo "$NAME is not running but $PIDFILE exists."
+		return 1
+        fi
+}
+
+start_daemon()
+{
+	VERSION="$1"
+	CONF="$2"
+	NAME="$3"
+	PIDFILE="$4"
+	DESC="$5"
+
+	shift 5
+	INTERFACES="$*"
+
+	test_config "$VERSION" "$CONF"
+	log_daemon_msg "Starting $DESC" "$NAME"
+
+	if [ -e "$DHCPD_PID" ]; then
+		log_failure_msg "dhcpd service already running (pid file $PIDFILE currenty exists)"
+		exit 1
+	fi
+
+	start-stop-daemon --start --quiet --pidfile $PIDFILE \
+		--exec /usr/sbin/dhcpd -- $VERSION -q -cf $CONF $INTERFACES
+	sleep 2
+
+	if check_status -q $PIDFILE $NAME; then
+		log_end_msg 0
+	else
+		log_failure_msg "check syslog for diagnostics."
+		log_end_msg 1
+		exit 1
+	fi
+}
+
+stop_daemon()
+{
+	if check_status -q $DHCPDv4_PID $NAME4; then
+		log_daemon_msg "Stopping $DESC4" "$NAME4"
+		start-stop-daemon --stop --quiet --pidfile $DHCPDv4_PID
+		log_end_msg $?
+		rm -f "$DHCPDv4_PID"
+	fi
+
+	if check_status -q $DHCPDv6_PID $NAME6; then
+		log_daemon_msg "Stopping $DESC6" "$NAME6"
+		start-stop-daemon --stop --quiet --pidfile $DHCPDv6_PID
+		log_end_msg $?
+		rm -f "$DHCPDv6_PID"
+	fi
 }
 
 case "$1" in
 	start)
-		test_config
-		if [ -e "$DHCPD_PID" ]; then
-			log_failure_msg "dhcpd service already running (pid file $DHCPD_PID currenty exists)"
-			exit 1
+		if test -n "$INTERFACESv4"; then
+			start_daemon "-4" "$DHCPDv4_CONF" "$NAME4" \
+				"$DHCPDv4_PID" "$DESC4" "$INTERFACESv4"
 		fi
-		log_daemon_msg "Starting $DESC" "$NAME"
-		start-stop-daemon --start --quiet --pidfile "$DHCPD_PID" \
-			--exec /usr/sbin/dhcpd -- \
-			-q $OPTIONS -cf "$DHCPD_CONF" -pf "$DHCPD_PID" $INTERFACES
-		sleep 2
-
-		if check_status -q; then
-			log_end_msg 0
-		else
-			log_failure_msg "check syslog for diagnostics."
-			log_end_msg 1
-			exit 1
+		if test -n "$INTERFACESv6"; then
+			start_daemon "-6" "$DHCPDv6_CONF" "$NAME6" \
+				"$DHCPDv6_PID" "$DESC6" "$INTERFACESv6"
 		fi
 		;;
 	stop)
-		log_daemon_msg "Stopping $DESC" "$NAME"
-		start-stop-daemon --stop --quiet --pidfile "$DHCPD_PID"
-		log_end_msg $?
-		rm -f "$DHCPD_PID"
+		stop_daemon
 		;;
 	restart | force-reload)
-		test_config
 		$0 stop
 		sleep 2
 		$0 start
@@ -108,9 +157,12 @@ case "$1" in
 		fi
 		;;
 	status)
-		echo -n "Status of $DESC: "
-		check_status -v
-		exit "$?"
+		echo -n "Status of $DESC4: "
+		check_status -v $DHCPv4_PID $NAME4
+		STATUSv4="$?"
+		echo -n "Status of $DESC6: "
+		check_status -v $DHCPv6_PID $NAME6
+		exit "$(($STATUSv4|$?))"
 		;;
 	*)
 		echo "Usage: $0 {start|stop|restart|force-reload|status}"
